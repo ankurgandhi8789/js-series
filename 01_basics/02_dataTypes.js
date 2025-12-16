f7f2e73771cca5bacde20a9a92560d045d462dc8
Ankur gandhi <168157565+ankurgandhi8789@users.noreply.github.com>
2025-12-16 17:36:19 +0000

    datatypesCommited

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all JS code as newer version
+// alert("this is use for popup ") // we are using nodejs , not browser
+ 
+console.log(3+3) // code readability should be high
+ console.log("Ankur")
+
+ let Name="Ankur gandhi"
+ let age="20"
+ let isLoggedIn=false 
+
+ // number => 2 to power 53
+ // bigint // BigInt is used to store very large integers beyond the safe limit of Number (2^53 - 1).
+ // string =>" "
+ // boolean => true/false
+ // null => standlone value
+ // undefined => 
+ // symbol => unique // Symbol is a primitive datatype used to create unique identifiers, even if values look the same.
+ //  example : 
+    const id1 = Symbol("id");
+    const id2 = Symbol("id");
+
+    console.log(id1 === id2); // false (symbols are always unique)
+ // object 
+    
+ //imp =>
+    console.log(typeof undefined) // undefined
+    console.log(typeof null) // object
+
+    console.log(typeof(Name))
