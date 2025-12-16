b529d05c2af1b36a392f0a0585f016aa7af93efd
Ankur gandhi <168157565+ankurgandhi8789@users.noreply.github.com>
2025-12-16 18:03:19 +0000

    conversionCommited

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score="33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score);
+
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggesIn = "ankur"
+// console.log(typeof isLoggesIn)
+
+let booleanIsLoggesIn = Boolean(isLoggesIn)
+// console.log(typeof booleanIsLoggesIn)
+// console.log(booleanIsLoggesIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "ankur" => true
+
+let someNumber =33
+console.log(someNumber)
+console.log(typeof someNumber)
+let stringNumber =String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
