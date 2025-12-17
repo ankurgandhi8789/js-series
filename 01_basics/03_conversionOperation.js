11c0bdf8949a62eace86360e8f16d1955541385d
Ankur gandhi <168157565+ankurgandhi8789@users.noreply.github.com>
2025-12-17 08:39:19 +0000

    07 Done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,9 +24,46 @@ let booleanIsLoggesIn = Boolean(isLoggesIn)
 // "ankur" => true
 
 let someNumber =33
-console.log(someNumber)
-console.log(typeof someNumber)
+// console.log(someNumber)
+// console.log(typeof someNumber)
 let stringNumber =String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ********************************** Operations ******************************
+
+let value=3;
+// console.log(value);
+let negValue= -value;
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="Ankur"
+let str2=" gandhi"
+let str3=str1+str2;
+// console.log(str3)
+
+// console.log("1" +2 +2);
+// console.log(2+2+"1");
+// console.log("23" *2)  // don't know
+
+// console.log(+true) // here true means (1) so, we increment 1 but value can't be change
+// console.log(true+) // this is not valid
+
+// console.log(+"")
+
+
+let num1,num2,num3
+num1=num2=num3=2+2
+// console.table([num1,num2,num3])
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter)
